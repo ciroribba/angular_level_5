3ac99957d1e8474b58281b0ee62eba1b7e227719
ciroribba <ciro.ribba@gmail.com>
2025-12-26 12:05:42 -0300

    add examples abstract control

--- src/app/auth/pages/register-page/register-page.ts
+++ src/app/auth/pages/register-page/register-page.ts
@@ -1,6 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { JsonPipe } from '@angular/common';
-import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { FormUtils } from '../../../utils/form-util';
 
 @Component({
@@ -8,20 +14,34 @@ import { FormUtils } from '../../../utils/form-util';
   imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.html',
 })
-export class RegisterPage { 
+export class RegisterPage {
   private fb = inject(FormBuilder);
   formUtils = FormUtils;
-  myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.pattern(FormUtils.namePattern)], []],
-    email: ['', [Validators.required, Validators.pattern(FormUtils.emailPattern)], []],
-    username: ['', [Validators.required, Validators.minLength(6), Validators.pattern(FormUtils.notOnlySpacesPattern)], []],
-    password: ['', [Validators.required, Validators.minLength(6)], []],
-    password2: ['', [Validators.required], []],
-  }, {
-    validators: [FormUtils.isFieldOneEqualFieldTwo('password', 'password2')]
-  });
-
-  
+  myForm: FormGroup = this.fb.group(
+    {
+      name: ['', [Validators.required, Validators.pattern(FormUtils.namePattern)], []],
+      email: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.emailPattern)],
+        [FormUtils.checkingServerResponse],
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(FormUtils.notOnlySpacesPattern),
+          FormUtils.notStrider,
+        ],
+        [],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)], []],
+      password2: ['', [Validators.required], []],
+    },
+    {
+      validators: [FormUtils.isFieldOneEqualFieldTwo('password', 'password2')],
+    }
+  );
 
   onSubmit() {
     console.log(this.myForm.value);
--- src/app/utils/form-util.ts
+++ src/app/utils/form-util.ts
@@ -1,5 +1,13 @@
 import { FormArray, FormGroup, ValidationErrors, AbstractControl } from '@angular/forms';
 
+async function sleep(): Promise<boolean> {
+  return new Promise(resolve => {
+    setTimeout(() => { 
+      resolve(true);
+    }, 2500);
+    });
+}
+
 export class FormUtils {
   static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)$';
   static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
@@ -14,8 +22,12 @@ export class FormUtils {
           return `Este campo debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
         case 'min':
           return `Este campo debe tener un valor mayor o igual a ${errors['min'].min}`;
+        case 'notStrider':
+          return 'No puede ser Strider';
         case 'email':
           return 'No es una dirección de email válida';
+        case 'emailTaken':
+          return 'Este email ya está registrado';
         case 'pattern':
           if(errors['pattern'].requiredPattern === FormUtils.emailPattern) {
             return 'No es una dirección de email válida';
@@ -54,5 +66,26 @@ export class FormUtils {
       const field2Value = formGroup.get(field2)?.value;
       return field1Value === field2Value ? null : { notEqual: true };
     }
+  }
+
+  static async checkingServerResponse(control: AbstractControl): Promise<ValidationErrors | null> {
+    await sleep();
+    const formValue = control.value;
+    if (formValue === 'hola@mundo.com') {
+      return {
+        emailTaken: true,
+      };
+    }
+    return null;
+  }
+
+  static notStrider(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (value === 'strider') {
+      return {
+        notStrider: true,
+      };
+    }
+    return null;
   }
 }

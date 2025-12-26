4a4357b91a03ef62729332fa446f8912da771500
ciroribba <ciro.ribba@gmail.com>
2025-12-26 14:02:51 -0300

    Complete service country e2e

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,8 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { CountryService } from '../services/country.service';
+import { Country } from '../interfaces/country.interface';
+import { switchMap, tap, filter } from 'rxjs';
 
 @Component({
   selector: 'country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.html',
 })
-export class CountryPage { }
+export class CountryPage {
+  fb = inject(FormBuilder);
+
+  countryService = inject(CountryService);
+
+  regions = signal(this.countryService.regions);
+
+  countriesByRegion = signal<Country[]>([]);
+  bordersByCountry = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+  onFormChanged = effect((onCleanup ) => {
+    const regionSuscriptions = this.onRegionChanged();
+    const countrySuscriptions = this.onCountryChanged();
+    onCleanup(() => {
+      regionSuscriptions.unsubscribe();
+      countrySuscriptions.unsubscribe();
+    });
+  });
+
+  onRegionChanged() {
+    return this.myForm.get('region')!.valueChanges
+    .pipe(
+      tap(() => this.myForm.get('country')!.setValue('')),
+      tap(() => this.myForm.get('border')!.setValue('')),
+      tap(() => {
+        this.bordersByCountry.set([]);
+        this.countriesByRegion.set([]);
+      }),
+      switchMap(region => this.countryService.getCountriesByRegion(region!)),
+    )
+    .subscribe( countries=> {
+      this.countriesByRegion.set(countries);
+    });
+  }
+
+  onCountryChanged() {
+    return this.myForm
+    .get('country')!
+    .valueChanges.pipe(
+      tap(() => this.myForm.get('border')!.setValue('')),
+      filter(value => value!.length > 0),
+      switchMap(alphaCode => this.countryService.getCountryByAlphaCode(alphaCode!)),
+      switchMap(country => this.countryService.getCountryNamesByCodeArray(country.borders!)),
+    )
+    .subscribe( borders=> {
+      this.bordersByCountry.set(borders);
+    });
+  }
+}
